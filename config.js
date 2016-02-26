c904242e98caa63700804c5afd155e2e1cde1d83
Websy <websy85@users.noreply.github.com>
2016-02-26 12:40:01 +0000

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  github:{
-    clientId: "c76a047f72b844447747", //to allow users to login with their git creds
-    secret: "831a8fa3baa8a273aa4addf005af84f1d6ec5948", //to allow users to login with their git creds
-    token: "1b20325b074263568e72572514c115e6f1a152e6" //for access from the connector itself
-  },
-  mongo: 'mongodb://principal:principal%@ds041404.mongolab.com:41404/heroku_s6fg7p6h'
-}
