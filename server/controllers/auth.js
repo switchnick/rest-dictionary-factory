42cbd2e7c083e7e387d9bf308b41515b5414b706
Nick Webster <nick.webster@qlikview.com>
2016-03-18 16:28:05 +0000

    restyle

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,6 @@
 module.exports = {
   isLoggedIn: function(req, res, next){
-    if(req.isAuthenticated()){
+    if(req.isAuthenticated() || req.url=="/public" || req.url.indexOf("summary")!=-1){
       console.log(req.user);
       console.log(req.url);
       console.log('authenticated');
