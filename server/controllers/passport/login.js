73acc48787bcc428fc28d7963629c0da32753a5b
Nick Webster <nick.webster@qlikview.com>
2016-03-23 16:54:02 +0000

    session fix for oauth

--- server/controllers/passport/login.js
+++ server/controllers/passport/login.js
@@ -1,14 +1,12 @@
 var GitHubStrategy    = require('passport-github').Strategy;
 
 module.exports = function(passport){
-  console.log('we got here too');
   passport.use(new GitHubStrategy({
       clientID: process.env.github_clientId,
       clientSecret: process.env.github_secret,
       scope: 'public_repo'
     },
     function(accessToken, refreshToken, profile, done) {
-      console.log('uugh');
       profile.token = accessToken;
       // asynchronous verification, for effect...
       process.nextTick(function () {
